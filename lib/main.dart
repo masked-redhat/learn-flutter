2f4e87bede4de1375f8d301ad55631c9c24b3d78
Zed <skhull.coding@proton.me>
2025-02-28 02:22:43 +0530

    learn: row and columns

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,19 @@ class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(
-          image: AssetImage('assets/images/image1.jpg'),
-          height: 300,
+      body: Container(
+        height: 200,
+        color: Colors.amber,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            // type of widgets, <Widget> is redundant if many types of widgets
+            Text('hello', style: TextStyle(fontSize: 24)),
+            Text('hello2', style: TextStyle(fontSize: 27)),
+            Text('hello3', style: TextStyle(fontSize: 30)),
+            Text('hello4', style: TextStyle(fontSize: 33)),
+          ],
         ),
       ),
     );
