7a227df06b604bae7859134f87e6e885c5efbfe6
Zed <skhull.coding@proton.me>
2025-03-02 10:01:05 +0530

    learn: gesture detector to make any element interactable

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,16 @@ class MainWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-    final double height = screenSize.height;
-    final double width = screenSize.width;
-
     return Scaffold(
-      body: Container(
-        height: height,
-        width: width,
-        color: Colors.amber,
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             // type of widgets, <Widget> is redundant if many types of widgets
-            Text('hello', style: TextStyle(fontSize: 24)),
+            GestureDetector(
+              onTap: () => print("Going home"),
+              child: Text('hello', style: TextStyle(fontSize: 24)),
+            ),
             Text('hello2', style: TextStyle(fontSize: 27)),
             Text('hello3', style: TextStyle(fontSize: 30)),
             Text('hello4', style: TextStyle(fontSize: 33)),
