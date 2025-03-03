b6318ca5a8744924a05ec256ee172e577da30b42
Zed <skhull.coding@proton.me>
2025-03-03 11:34:23 +0530

    chore: unneccsary import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:app1/util/util.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
